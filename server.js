e4724824bc3c168915f418ef68417720dd1e9faf
Etanami Olatunji <etanamiolatunji@gmail.com>
2024-06-24 10:51:44 +0100

    feat: set up server

---
+++ server.js
@@ -0,0 +1,9 @@
+import express from "express";
+
+const app = express();
+
+const PORT = 3000 || process.env.PORT;
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
