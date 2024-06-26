5ab8fe0c7f7da2f34cc375e675d4be56dd37df46
Etanami Olatunji <etanamiolatunji@gmail.com>
2024-06-26 16:47:05 +0100

    refactor: naming convention

--- server.js
+++ server.js
@@ -6,19 +6,19 @@ const PORT = 3000 || process.env.PORT;
 
 // GET route for user query
 app.get("/api/hello", async (req, res) => {
-  const visitor_name = req.query.visitor_name;
-  const client_ip = req.ip;
+  const visitorName = req.query.visitor_name;
+  const clientIp = req.ip;
 
   // fetch user location using ipapi
   try {
-    const fetchData = await axios.get(`https://ipapi.co/${client_ip}/json/`);
+    const fetchData = await axios.get(`https://ipapi.co/${clientIp}/json/`);
     const location = fetchData.data;
     //console.log(location);
 
     res.status(200).json({
-      client_ip,
+      clientIp,
       location: location || "Unknown location",
-      greeting: `Hello, ${visitor_name}!`,
+      greeting: `Hello, ${visitorName}!`,
     });
   } catch (err) {
     console.error("Error occurred:", err)
